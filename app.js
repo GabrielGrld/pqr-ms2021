2de92a86d9a3db958da2d5051d9b49ed7a5f0613
GabrielGrld <gbrlgrld@gmail.com>
2021-11-25 16:23:04 -0500

    create Model and Schema for MongoDB

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/pqrDB');
+
+
+
+
+//Model for each Article
+const pqrSchema = {
+  type : String,   // Type.  P: Petición, Q: Queja, R: Reclamo
+  user: String,   // Usuario que realizá la PQR
+  product_id: Number,  //Producto relacionado con la PQR
+  content: String,   // Contenido de la PQR
+  date: {type: Date, default: Date.now }   //Fecha en la que se realiza la PQR
+};
+
+//Schema for each PQR
+ const pqr = mongooe.model("pqr", pqrSchema);
