793df94f2b1214001c39b14d56ae2b53ecf4769d
GabrielGrld <gbrlgrld@gmail.com>
2021-11-25 17:57:37 -0500

    add post route to API

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require('mongoose');
 
 
@@ -22,11 +21,49 @@ mongoose.connect('mongodb://localhost:27017/pqrDB');
 //Model for each Article
 const pqrSchema = {
   type : String,   // Type.  P: Petición, Q: Queja, R: Reclamo
-  user: String,   // Usuario que realizá la PQR
+  user: Number,   // Usuario que realizá la PQR
   product_id: Number,  //Producto relacionado con la PQR
   content: String,   // Contenido de la PQR
   date: {type: Date, default: Date.now }   //Fecha en la que se realiza la PQR
 };
 
 //Schema for each PQR
- const pqr = mongooe.model("pqr", pqrSchema);
+ const Pqr = mongoose.model("pqr", pqrSchema);
+///////////////////////Request Targettomh All the PQRs ////////////////////////////
+
+app.route("/pqr")
+.get(function(req, res){
+  Pqr.find(function(err, foundPQRs){
+    if(!err){
+      res.send(foundPQRs);
+    }else{
+      res.send(err);
+    }
+  });
+}).post(  bodyParser.text({ type: ['json', 'text']}),
+ function(req, res ){
+   const pqrToSave = new Pqr ({
+     type : JSON.parse(req.body).type,
+     user: parseInt(JSON.parse(req.body).user),
+     product_id: parseInt(JSON.parse(req.body).product_id),
+     content: JSON.parse(req.body).content,
+   });
+   pqrToSave.save(function(err, result){
+     if (!err) {
+      res.send(result);
+    } else {
+      res.send(err);
+    }
+   });
+});
+
+
+
+
+
+
+
+
+ app.listen(3000, function() {
+   console.log("Server started on port 3000");
+ });
